c7e52daf67870af27a992042a63220313adde68d
swlho <swlho87@gmail.com>
2024-02-23 10:29:22 +0000

    completes task 18 PATCH /api/comments/:comment_id

--- models/comments.model.js
+++ models/comments.model.js
@@ -14,4 +14,20 @@ function removeCommentById(comment_id){
     })
 }
 
-module.exports = {removeCommentById}
+function updateCommentById(updateVotesValue,comment_id){
+    const {inc_votes} = updateVotesValue
+    const queryVals = [inc_votes,comment_id]
+
+    const sqlQueryStr = `
+    UPDATE comments
+    SET votes = votes + $1
+    WHERE comment_id = $2
+    RETURNING *`
+
+    return db.query(sqlQueryStr, queryVals)
+    .then(({rows})=>{
+        return rows[0]
+    })
+}
+
+module.exports = {removeCommentById, updateCommentById}
